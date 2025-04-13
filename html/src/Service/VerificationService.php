1f5f6152345800a760ff4dddee32429b723219be
Gazizov Niyaz <niaz@mail.ru>
2025-04-13 22:50:37 +0500

    Сам сервис верификации кода

--- html/src/Service/VerificationService.php
+++ html/src/Service/VerificationService.php
@@ -2,18 +2,30 @@
 
 namespace App\Service;
 
+use App\Entity\BlockPhone;
+use App\Entity\User;
 use App\Entity\VerificationCode;
+use App\Repository\BlockPhoneRepository;
 use App\Repository\UserRepository;
 use App\Repository\VerificationCodeRepository;
 use Random\RandomException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class VerificationService
 {
     private $userRepository;
     private $verificationCodeRepository;
+    private $validator;
 
-    public function __construct(UserRepository $userRepository, VerificationCodeRepository $verificationCodeRepository)
-    {
+    public function __construct(
+        ValidatorInterface $validator,
+        UserRepository $userRepository,
+        VerificationCodeRepository $verificationCodeRepository,
+        private readonly BlockPhoneRepository $blockPhoneRepository,
+    ) {
+        $this->validator = $validator;
         $this->userRepository = $userRepository;
         $this->verificationCodeRepository = $verificationCodeRepository;
     }
@@ -21,8 +33,14 @@ class VerificationService
     /**
      * @throws RandomException
      */
-    public function requestVerificationCode(string $phone)
+    public function requestVerificationCode(string $phone): VerificationCode
     {
+        $this->isBlocked($phone);
+
+        if (3 < $this->verificationCodeRepository->getCount($phone)) {
+            $this->blockPhoneRepository->insert(new BlockPhone($phone));
+        }
+
         if ($v = $this->verificationCodeRepository->findByPhone($phone)) {
             $code = new VerificationCode($v['phone']);
             $code->setCode($v['code']);
@@ -33,11 +51,74 @@ class VerificationService
         $verificationCode = new VerificationCode($phone);
         $this->verificationCodeRepository->save($verificationCode);
         return $verificationCode;
-        // логика для генерации и отправки кода, проверки блокировки, сохранения кода
     }
 
-    public function verifyCode(string $phone, string $code)
+    private function isBlocked(string $phone): void
     {
-        // логика для проверки кода, регистрации/авторизации пользователя
+        if ($block = $this->blockPhoneRepository->isBlocked($phone)) {
+            $data = $block->getBlockAt()->format('Y-m-d H:i:s');
+            throw new \Exception(
+                "Вы заблокированы до {$data}",
+                Response::HTTP_LOCKED,
+            );
+        }
+    }
+
+    public function verifyCode(?string $token, ?string $code): VerificationCode
+    {
+        $constraints = new Constraints\Collection([
+            'code' => [
+                new Constraints\NotBlank(message: 'Код не должен быть пустым'),
+                new Constraints\Length(
+                    4,
+                    exactMessage: 'Код должен быть 4 значным',
+                ),
+            ],
+            'token' => [
+                new Constraints\NotBlank(message: 'Вы не передали токен'),
+            ],
+        ]);
+
+        $errors = $this->validator->validate(
+            ['code' => $code, 'token' => $token],
+            $constraints,
+        );
+
+        if (count($errors) > 0) {
+            throw new \Exception(
+                $errors[0]->getMessage(),
+                Response::HTTP_BAD_REQUEST,
+            );
+        }
+
+        if ($v = $this->verificationCodeRepository->findByTokenAndCode(
+            $token,
+            $code,
+        )
+        ) {
+            if ($user = $this->userRepository->findByPhone($v['phone'])) {
+                $id = $user->getId();
+                $return = [
+                    'message' => 'Вы успешно авторизовались',
+                    'id'      => $id,
+                ];
+            } else {
+                $user = new User($v['phone']);
+                $id = $this->userRepository->insert($user)->getId();
+                $return = [
+                    'message' => 'Вы успешно зарегистрировались',
+                    'id'      => $id,
+                ];
+            }
+//            $this->verificationCodeRepository->delete($v['id']);
+
+        } else {
+            throw new \Exception(
+                'Введенный код неверен',
+                Response::HTTP_UNAUTHORIZED,
+            );
+        }
+
+        return $return;
     }
 }
